222f0487b46dd561c7ae33040d8a22771a150696
mirthfulLee <lihuan0530@gmail.com>
2023-08-19 08:22:22 +0000

    add comments&gitignore

--- CGImysql/sql_connection_pool.h
+++ CGImysql/sql_connection_pool.h
@@ -1,60 +1,66 @@
 #ifndef _CONNECTION_POOL_
 #define _CONNECTION_POOL_
 
-#include <stdio.h>
-#include <list>
-#include <mysql/mysql.h>
 #include <error.h>
+#include <mysql/mysql.h>
+#include <stdio.h>
 #include <string.h>
+
 #include <iostream>
+#include <list>
 #include <string>
+
 #include "../lock/locker.h"
 #include "../log/log.h"
 
 using namespace std;
 
-class connection_pool
-{
-public:
-	MYSQL *GetConnection();				 //获取数据库连接
-	bool ReleaseConnection(MYSQL *conn); //释放连接
-	int GetFreeConn();					 //获取连接
-	void DestroyPool();					 //销毁所有连接
+class connection_pool {
+ public:
+  MYSQL *GetConnection();               //获取数据库连接
+  bool ReleaseConnection(MYSQL *conn);  //释放连接
+  int GetFreeConn();                    //获取连接
+  void DestroyPool();                   //销毁所有连接
 
-	//单例模式
-	static connection_pool *GetInstance();
+  //单例模式
+  static connection_pool *GetInstance();
 
-	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log); 
+  void init(string url, string User, string PassWord, string DataBaseName,
+            int Port, int MaxConn, int close_log);
 
-private:
-	connection_pool();
-	~connection_pool();
+ private:
+  connection_pool();
+  ~connection_pool();
 
-	int m_MaxConn;  //最大连接数
-	int m_CurConn;  //当前已使用的连接数
-	int m_FreeConn; //当前空闲的连接数
-	locker lock;
-	list<MYSQL *> connList; //连接池
-	sem reserve;
+  int m_MaxConn;   //最大连接数
+  int m_CurConn;   //当前已使用的连接数
+  int m_FreeConn;  //当前空闲的连接数
+  locker lock;
+  list<MYSQL *> connList;  //连接池
+  sem reserve;
 
-public:
-	string m_url;			 //主机地址
-	string m_Port;		 //数据库端口号
-	string m_User;		 //登陆数据库用户名
-	string m_PassWord;	 //登陆数据库密码
-	string m_DatabaseName; //使用数据库名
-	int m_close_log;	//日志开关
+ public:
+  string m_url;           //主机地址
+  string m_Port;          //数据库端口号
+  string m_User;          //登陆数据库用户名
+  string m_PassWord;      //登陆数据库密码
+  string m_DatabaseName;  //使用数据库名
+  int m_close_log;        //日志开关
 };
 
-class connectionRAII{
+// ?这个类是干嘛的?
+// *RAII: Resource Acquisition Is Initialization-资源获取即初始化
+// *使用RAII即可让操作系统根据局部变量的生命周期自动销毁资源, 而不再需要手动销毁
+class connectionRAII {
+ public:
+	// *构造函数实现资源(MySQL连接)的获取
+  connectionRAII(MYSQL **con, connection_pool *connPool);
+	// *当局部变量生命周期结束时执行, 释放资源(MySQL连接)
+  ~connectionRAII();
 
-public:
-	connectionRAII(MYSQL **con, connection_pool *connPool);
-	~connectionRAII();
-	
-private:
-	MYSQL *conRAII;
-	connection_pool *poolRAII;
+ private:
+  MYSQL *conRAII;
+  connection_pool *poolRAII;
 };
 
 #endif
--- timer/lst_timer.h
+++ timer/lst_timer.h
@@ -1,102 +1,108 @@
 #ifndef LST_TIMER
 #define LST_TIMER
 
-#include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/epoll.h>
-#include <fcntl.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <assert.h>
-#include <sys/stat.h>
-#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <pthread.h>
+#include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
 #include <sys/mman.h>
-#include <stdarg.h>
-#include <errno.h>
-#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/uio.h>
-
+#include <sys/wait.h>
 #include <time.h>
+#include <unistd.h>
+
 #include "../log/log.h"
 
 class util_timer;
 
-struct client_data
-{
-    sockaddr_in address;
-    int sockfd;
-    util_timer *timer;
+struct client_data {
+  sockaddr_in address;
+  int sockfd;
+  // FIXME: 啥意思? 相互引用??
+  util_timer *timer;
 };
 
-class util_timer
-{
-public:
-    util_timer() : prev(NULL), next(NULL) {}
+// *升序计时器链表的结点类
+class util_timer {
+ public:
+  util_timer() : prev(NULL), next(NULL) {}
 
-public:
-    time_t expire;
-    
-    void (* cb_func)(client_data *);
-    client_data *user_data;
-    util_timer *prev;
-    util_timer *next;
+ public:
+  time_t expire;
+
+  // 一个函数指针, 任务回调函数
+  // *回调函数处理的客户数据, 由定时器的执行者传递给回调函数
+  void (*cb_func)(client_data *);
+  client_data *user_data;
+  util_timer *prev;
+  util_timer *next;
 };
 
-class sort_timer_lst
-{
-public:
-    sort_timer_lst();
-    ~sort_timer_lst();
+// *升序计时器链表, 双向
+class sort_timer_lst {
+ public:
+  sort_timer_lst();
+  ~sort_timer_lst();
 
-    void add_timer(util_timer *timer);
-    void adjust_timer(util_timer *timer);
-    void del_timer(util_timer *timer);
-    void tick();
+  // *添加计时器的接口
+  void add_timer(util_timer *timer);
+  void adjust_timer(util_timer *timer);
+  void del_timer(util_timer *timer);
+  // *SIGALRM信号每次被触发就在信号处理函数中执行依次tick函数, 从而处理到期任务
+  void tick();
 
-private:
-    void add_timer(util_timer *timer, util_timer *lst_head);
+ private:
+  // *重载的辅助函数, 将目标定时器插入到lst_head后面的位置
+  void add_timer(util_timer *timer, util_timer *lst_head);
 
-    util_timer *head;
-    util_timer *tail;
+  util_timer *head;
+  util_timer *tail;
 };
 
-class Utils
-{
-public:
-    Utils() {}
-    ~Utils() {}
+class Utils {
+ public:
+  Utils() {}
+  ~Utils() {}
 
-    void init(int timeslot);
+  void init(int timeslot);
 
-    //对文件描述符设置非阻塞
-    int setnonblocking(int fd);
+  //对文件描述符设置非阻塞
+  int setnonblocking(int fd);
 
-    //将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
-    void addfd(int epollfd, int fd, bool one_shot, int TRIGMode);
+  //将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
+  // *EPOLLONESHOT避免不同线程同时处理一个连接socket上的事件
+  // *类似一个互斥锁, 在处理完事件后需要立即重置这个EPOLLONESHOT事件
+  void addfd(int epollfd, int fd, bool one_shot, int TRIGMode);
 
-    //信号处理函数
-    static void sig_handler(int sig);
+  //信号处理函数
+  static void sig_handler(int sig);
 
-    //设置信号函数
-    void addsig(int sig, void(handler)(int), bool restart = true);
+  //设置信号函数
+  void addsig(int sig, void(handler)(int), bool restart = true);
 
-    //定时处理任务，重新定时以不断触发SIGALRM信号
-    void timer_handler();
+  //定时处理任务，重新定时以不断触发SIGALRM信号
+  void timer_handler();
 
-    void show_error(int connfd, const char *info);
+  void show_error(int connfd, const char *info);
 
-public:
-    static int *u_pipefd;
-    sort_timer_lst m_timer_lst;
-    static int u_epollfd;
-    int m_TIMESLOT;
+ public:
+  static int *u_pipefd;
+  sort_timer_lst m_timer_lst;
+  static int u_epollfd;
+  int m_TIMESLOT;
 };
-
+// *定时器回调函数, 它删除非活动连接socket上的注册事件, 并关闭之
 void cb_func(client_data *user_data);
 
 #endif
